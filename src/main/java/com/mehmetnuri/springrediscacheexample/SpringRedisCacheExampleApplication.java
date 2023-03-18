7ce6a107c0a32a796630f54019cc579199585610
Mehmet Nuri <info@mehmetnuri.net>
2023-03-18 21:18:34 +0300

    Initial commit

---
+++ src/main/java/com/mehmetnuri/springrediscacheexample/SpringRedisCacheExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mehmetnuri.springrediscacheexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisCacheExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisCacheExampleApplication.class, args);
+    }
+
+}
